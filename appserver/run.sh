58a6c80aed5915fbd65677cb53d408e3726d4723
Julian de Almeida Santos <julian.santos.info@gmail.com>
2024-07-12 17:43:24 -0300

    Atualiza script run para container appserver.

--- appserver/run.sh
+++ appserver/run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run -d --name totvs_appserver --network totvs -p 1234:1234 -p 12345:12345 -p 8088:8088 --ulimit nofile=65536:65536 juliansantosinfo/totvs_appserver:latet
+docker run -d --name totvs_appserver --network totvs -p 1234:1234 -p 12345:12345 -p 8088:8088 --ulimit nofile=65536:65536 juliansantosinfo/totvs_appserver:latest
